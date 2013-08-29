b4eac8ac18353789e4ba09d5421aedfd72ab2a5b
Oskar Petersons <a.more.reliable.source@gmail.com>
2013-08-29 18:53:29 -0400

    solution for Problem 31

--- Problem031.py
+++ Problem031.py
@@ -7,23 +7,30 @@
 
     It is possible to make £2 in the following way:
 
-    1£1 + 150p + 220p + 15p + 12p + 31p
+    1x£1 + 1x50p + 2x20p + 1x5p + 1x2p + 3x1p
 
     How many different ways can £2 be made using any number of coins?
 '''
 
-def isFifthDigSum(number):
-    if number==sum([int(x)**5 for x in str(number)]):
-        return True
-    else: return False
+britishCoins = [200,100,50,20,10,5,2,1]
 
-fifthDigSums=[]
+def fillDown(valueLeft,coinValues,coinIndex=0):
+    if coinIndex >= len(coinValues):
+        return 0
+    coin = coinValues[coinIndex]
+    if valueLeft/coin < 1:
+        return fillDown(valueLeft,coinValues,coinIndex+1)
+    else:
+        uniqueFills = 0
+        for coinCount in range(valueLeft//coin+1):
+            uniqueFills += fillDown(valueLeft-coin*coinCount,coinValues,coinIndex+1)
+        if valueLeft%coin == 0:
+            uniqueFills += 1
+        return uniqueFills
 
-for x in range(2,500000):
-    if isFifthDigSum(x): fifthDigSums.append(x)
+        
+possibleWays = fillDown(200,britishCoins)
 
-print(sum(fifthDigSums))
+print(possibleWays)
 
-
-
-# A= 443839
+# A=73682
